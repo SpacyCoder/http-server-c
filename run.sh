4841a6e6b35abf4f832a76db7840121d8617930c
= <sral_h@hotmail.com>
2019-04-17 20:01:56 +0200

    chore: remove capabilities and cgroups example

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ docker stop cserver
 gcc server.c -o server -Wall -Wextra -Werror -static
 docker build -t c_server .
 
-docker run --name="cserver" --rm -p 8080:80 --cap-drop CHOWN --pids-limit 100 -v $(pwd)/www:/var/www c_server
+docker run --name="cserver" --rm -p 8080:80 -v $(pwd)/www:/var/www c_server
